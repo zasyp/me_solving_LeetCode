e71d3726bb34bffc51a3154fef3f4d6b5949240d
zasyp <zasypkin4514@gmail.com>
2024-12-11 16:34:15 +0300

    some problems

--- maximum depth of binary tree.py
+++ maximum depth of binary tree.py
@@ -5,4 +5,20 @@ class TreeNode(object):
         self.left = left
         self.right = right
 
-def maxDepth(self, root):
+def maxDepth(root):
+    if not root:
+        return 0
+
+    stack = [(root, 0)]
+    max_depth = 0
+
+    while stack:
+        node, depth = stack.pop()
+        max_depth = max(max_depth, depth)
+        if node.right:
+            stack.append((node.right, depth + 1))
+        if node.left:
+            stack.append((node.left, depth + 1))
+
+    return max_depth + 1
+
