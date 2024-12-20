64e76eab978861764a527c309f483d1539fe599a
zasyp <zasypkin4514@gmail.com>
2024-12-21 00:27:28 +0300

    2 new

--- contest 1/Remove Digit From Number to Maximize Result.py
+++
@@ -1,6 +0,0 @@
-def removeDigit(number: str, digit: str) -> str:
-    dump_list = []
-    while True:
-        for char in number:
-            if char == digit:
-                number.
---
+++ top interview questions/easy/single number.py
@@ -0,0 +1,3 @@
+from typing import List
+
+def singleNumber(nums: List[int]) -> int:
